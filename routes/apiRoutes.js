692e38e16290f16a6d8485220a4084d3d6278a21
Baba II <127648521+allister-seras@users.noreply.github.com>
2023-08-30 02:16:16 -0700

    completed delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,35 @@
-const fs = require("fs")
-const path = require('path');
-const notes = require("../db/db.json")
-const express = require("express")
-const router = express.Router()
+const fs = require("fs");
+const path = require("path");
+const notes = require("../db/db.json");
+const express = require("express");
+const router = express.Router();
 
 const saveNotes = () => {
-    fs.writeFileSync(path.resolve(__dirname, "../db/db.json"), JSON.stringify(notes))
-}
+  fs.writeFileSync(
+    path.resolve(__dirname, "../db/db.json"),
+    JSON.stringify(notes)
+  );
+};
 
 router.get("/notes", (req, res) => {
-    res.json(notes)
-})
+  let notesData = notes.map((note, index) => ({
+    ...note,
+    id: index
+  }));
+  console.log(notesData);
+  res.json(notes);
+});
 
 router.post("/notes", (req, res) => {
+  notes.push(req.body);
+  saveNotes(notes);
+  res.json(notes);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  notes.splice(req.params.id, 1)
+  saveNotes()
+  res.json()
 })
 
 module.exports = router;
