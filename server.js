829689e8464fbda57d190687f96c4934da23a689
Baba II <127648521+allister-seras@users.noreply.github.com>
2023-08-29 23:13:07 -0700

    setting up starter code and node modules

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+  // Send a message to the client
+  res.json(`${req.method} request received to get notes`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
+});
+
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a notes`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+
+    fs.readFile('./db/db.json', function(err, data) {
+      if (err) throw err;
+
+      var arrayOfObjects = JSON.parse(data);
+      arrayOfObjects.push(newNote);
+
+      // Write the string to a file
+      fs.writeFile(`./db/db.json`, noteString, (err) =>
+        err
+          ? console.error(err)
+          : console.log(
+              `Note for ${newNote.title} has been written to JSON file`
+            )
+      );
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
